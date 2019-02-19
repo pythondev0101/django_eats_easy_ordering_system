78ad15891b88b661679755ff0937e3045ba20288
pythondev0101 <rmontemayor0101@gmail.com>
2019-02-20 00:28:21 +0800

    Fix templates and api

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
-    path('auth/',include('social_django.urls', namespace="social")),
+    path('auth/', include('social_django.urls', namespace="social")),
 ]
