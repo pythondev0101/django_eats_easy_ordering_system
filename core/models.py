bd0b8207cc4c8309b1672bd6cb62830229e39df0
pythondev0101 <rmontemayor0101@gmail.com>
2019-03-29 00:19:54 +0800

    merge admin and custom

--- core/models.py
+++ core/models.py
@@ -42,6 +42,8 @@ class Product(models.Model):
     def get_absolute_url(self):
         pass
 
+    def __unicode__(self):
+        return self.name
 
 class Supplier(models.Model):
     """Blueprint for Supplier model"""
--- human_resource/urls.py
+++ human_resource/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.HRView.as_view(), name='human_resource'),
+    path('createorder/',views.CreateWeekOrderView.as_view(),name='hr_create_order')
 ]
