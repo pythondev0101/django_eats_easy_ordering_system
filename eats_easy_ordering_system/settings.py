dae2d96e97c141d6854d8725de74ff54bb5b7f44
pythondev0101 <rmontemayor0101@gmail.com>
2019-04-13 00:56:49 +0800

    up:

--- eats_easy_ordering_system/settings.py
+++ eats_easy_ordering_system/settings.py
@@ -88,14 +88,8 @@ WSGI_APPLICATION = 'eats_easy_ordering_system.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_eats_easy',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
